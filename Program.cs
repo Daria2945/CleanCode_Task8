49058e71ec26bb9fffc593efc0a8929ff04026b9
Daria2945 <148208175+Daria2945@users.noreply.github.com>
2025-07-02 15:00:32 +0300

    Update 1

--- Program.cs
+++ Program.cs
@@ -3,18 +3,54 @@
 class Player
 {
     private readonly Movement _movement;
+    private readonly Stats _stats;
+
     private Weapon _weapon;
 
-    public string Name { get; private set; }
+    public Player(Movement movement, Stats stats)
+    {
+        _movement = movement ?? throw new ArgumentNullException(nameof(movement));
+        _stats = stats ?? throw new ArgumentNullException(nameof(stats));
+    }
 
-    public int Age { get; private set; }
+    public void SetWeapon(Weapon weapon)
+    {
+        _weapon = weapon ?? throw new ArgumentNullException(nameof(weapon));
+    }
+}
+
+public class Stats
+{
+    public Stats(string name, int age)
+    {
+        if (age <= 0)
+            throw new ArgumentOutOfRangeException(nameof(age));
+
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        Age = age;
+    }
+
+    public readonly string Name { get; private set; }
+
+    public readonly int Age { get; private set; }
 }
 
 public class Weapon
 {
-    public int Damage { get; private set; }
+    private readonly int _damage;
+    private readonly float _cooldown;
 
-    public float Cooldown { get; private set; }
+    public Weapon(int damage, float cooldown)
+    {
+        if (damage <= 0)
+            throw new ArgumentOutOfRangeException(nameof(damage));
+
+        if (cooldown <= 0)
+            throw new ArgumentOutOfRangeException(nameof(cooldown));
+
+        _damage = damage;
+        _cooldown = cooldown;
+    }
 
     public void Attack()
     {
@@ -29,14 +65,27 @@ public class Weapon
 
 public class Movement
 {
-    public float Speed { get; private set; }
+    private readonly InputReder _inputReader;
+    private readonly float _speed;
 
-    public float DirectionX { get; private set; }
+    public Movement(InputReder inputReader, float speed)
+    {
+        if (speed <= 0)
+            throw new ArgumentOutOfRangeException(nameof(speed));
 
-    public float DirectionY { get; private set; }
+        _inputReader = inputReader ?? throw new ArgumentNullException(nameof(inputReader));
+        _speed = speed;
+    }
 
     public void Move()
     {
         //Do move
     }
+}
+
+public class InputReder
+{
+    public float DirectionX { get; private set; }
+
+    public float DirectionY { get; private set; }
 }
